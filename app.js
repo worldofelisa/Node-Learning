f97955524fc6725bbc391fcf6e1458b603a6109d
worldofelisa <worldofelisa@gmail.com>
2022-03-18 13:37:20 +0000

    Interacts with API to now get a cat gif, which currently outputs as an encrypted image in terminal. Posts Hello world still to terminal.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const https = require('https')
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -6,9 +7,32 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+
+    res.end("Hello World");
 });
 
+const options = {
+    hostname: 'cataas.com',
+    port: 443,
+    path:'/cat/gif',
+    method: 'GET',
+}
+
+const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+
+    res.on('data', d => {
+        process.stdout.write(d)
+    })
+})
+
+req.on('error', error => {
+    console.error(error)
+})
+
+req.end()
+
 server.listen(port, hostname, () => {
-    console.log('Server running at http://${hostname}:${port}/');
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
+
